3e9878b4144535f73aa46218d188ffc7e979e64a
Alex Alan Nunes <aalannunesdelima@apple.com>
2023-05-06 12:15:15 -0300

    feat: app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Flex } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import { MenuTables } from "./components/navbar";
+import { Header } from "./components/header";
+import { TableListProvider } from "./context";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableListProvider>
+      <Flex minH="100vh">
+        <MenuTables />
+        <Flex direction="column" flex={1}>
+          <Header />
+          <Container maxW={"container.lg"} pt={4}>
+            <Outlet />
+          </Container>
+        </Flex>
+      </Flex>
+    </TableListProvider>
   );
 }
-
-export default App;
